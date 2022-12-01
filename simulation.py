655e1a5de496cbb228866a01f2d69e89958b1288
niclasflach <niclas@flacken.com>
2022-12-01 12:35:09 +0100

    Now Collision detection works. Next step is to remove foods that has been collided with and give the blobs new life.

--- simulation.py
+++ simulation.py
@@ -10,6 +10,7 @@ green = 180,250,180
 screen = pygame.display.set_mode(size)
 blobs = []
 foods = []
+eaten_food = []
 
 
 class Blob:
@@ -26,10 +27,12 @@ class Blob:
         print(self.direction * 5)
         # self.direction = random.randint(0,10) - 5, random.randint(0,10) - 5
     def __del__(self):
-      print('Inside the destructor')
-      print('Object gets destroyed')
+      # print('Inside the destructor')
+      # print('Object gets destroyed')
+      pass
     def draw(self):
-        pygame.draw.circle(screen, (black), self.position, self.size, 0)
+        #self.sprite to use with collision detection
+        self.sprite = pygame.draw.circle(screen, (black), self.position, self.size, 0)
         pygame.draw.line(screen, black, self.position,  (self.size * 2 * self.direction) + self.position  )
     def look_for_food(self):
         pass
@@ -45,20 +48,36 @@ class Blob:
         self.position = self.position + self.direction * self.speed
     def age(self):
         self.life -= 1
+    def collision_detection(self):
+        #Most likely need to put all food sprites in a group or something for this.
+        #investigation best approach
+        #pygame.sprite.collide_circle() is one approach
+        
+        for food in range(len(foods)):
+            # print(foods[food].sprite)
+            if self.sprite.colliderect(foods[food].sprite):
+                print("Collision")
+            # collisions = pygame.sprite.spritecollide(self.sprite.rect, foods[food].sprite.rect, False, pygame.sprite.collide_circle)
+            # eaten_food.append(foods[food])
+            # self.life += 1000
+            
+        pass
         
 class Food:
+    def __init__(self,position, size):
+         self.sprite = pygame.sprite.Sprite()
     def __init__(self) -> None:
         self.position = pygame.Vector2(random.randint(0, width), random.randint(0,height))
         self.size = 3
     
     def draw(self):
-        pygame.draw.circle(screen, (green), self.position, self.size, 0)
+        self.sprite = pygame.draw.circle(screen, (green), self.position, self.size, 0)
+        # print(self.sprite)
         
     
+         
         
-            
-        
-
+# Function to generate blobs and put them in a list
 def generate_Blobs(number):
     generated = 0
     while generated < number:
@@ -73,7 +92,13 @@ def generate_Food(number):
     while generated < number:
         foods.append(Food())
         generated += 1
-    return 
+    return
+
+def remove_eaten_food():
+    for food in eaten_food:
+        foods.pop(food)
+
+    pass 
 
 def remove_dead_blobs():
     blobs_to_kill = []
@@ -96,15 +121,17 @@ while True:
         if event.type == pygame.QUIT: sys.exit()
     screen.fill(grey)
     
+    for food in foods:
+        food.draw()           
+    
     for blob in blobs:
         blob.look_for_food()
         blob.move()
         blob.draw()
+        blob.collision_detection() 
         blob.age()
                  
         #print(blob.life)
-    for food in foods:
-        food.draw()            
     remove_dead_blobs()
     
     pygame.display.flip()
